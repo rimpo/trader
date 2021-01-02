17e2cf0c7b57f34c1c838cfd34a75caab1073898
Manoj Kumar Bisht <manojbisht5@gmail.com>
2021-01-02 10:55:55 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,73 @@
+import json
+import logging
+from datetime import datetime
+from dataclasses import dataclass
+from kiteconnect import KiteTicker
+from kiteconnect import KiteConnect
+from config import API_KEY, ACCESS_TOKEN, INSTRUMENT_FILE
+
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger('generate_instruments')
+
+def load_instrument(exchange: str) -> dict:
+    with open(INSTRUMENT_FILE) as json_file:
+        instruments = json.load(json_file)
+        return instruments
+
+
+@dataclass(frozen=True)
+class Candle:
+    open_price: float
+    close_price: float
+    high_price: float
+    low_price: float
+    tick_count: int
+    start_time: datetime
+    last_traded_time: datetime
+
+# 256265
+# 341249
+subscribed_tokens = [256265, ]
+# 69121
+
+
+if __name__ == "__main__":
+
+    kite = KiteConnect(API_KEY)
+    kite.set_access_token(ACCESS_TOKEN)
+
+    kws = KiteTicker(API_KEY, ACCESS_TOKEN)
+
+    def on_ticks(ws, ticks):
+        # Callback to receive ticks.
+        logging.debug("Ticks: {}".format(ticks))
+
+    def on_connect(ws, response):
+        # Callback on successful connect.
+        # Subscribe to a list of instrument_tokens (RELIANCE and ACC here).
+        # ws.subscribe(subscribed_tokens)
+
+        # Set RELIANCE to tick in `full` mode.
+        # ws.set_mode(ws.MODE_FULL, subscribed_tokens)
+        logger.info(f"token: {subscribed_tokens} subscribed")
+
+    def on_close(ws, code, reason):
+        # On connection close stop the main loop
+        # Reconnection will not happen after executing `ws.stop()`
+        ws.stop()
+        logger.error("on_close called")
+
+    def on_reconnect(ws, attemps_count):
+        logger.warning(f"reconnect attempt {attemps_count}")
+
+    # Assign the callbacks.
+    kws.on_ticks = on_ticks
+    kws.on_connect = on_connect
+    kws.on_close = on_close
+
+    # Infinite loop on the main thread. Nothing after this will run.
+    # You have to use the pre-defined callbacks to manage subscriptions.
+    kws.connect()
+
+
